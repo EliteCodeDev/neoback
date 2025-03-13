42ddab6b3f52dfed1aaae2be8abf66ee39571fb3
NeocapitalDev <neocapital2025@gmail.com>
2025-03-13 18:42:28 -0500

    cambio

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -908,6 +908,7 @@ export interface ApiWithdrawWithdraw extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    estado: Schema.Attribute.Enumeration<['proceso', 'pagado', 'cancelado']>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
